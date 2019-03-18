f298e7be8a77eccb8e0f9acb4840b0f5ad860cce
Nguyen Kim Tu <tunk@saisystem.vn>
2019-03-18 18:47:27 +0700

    bai12

---
+++ TuNK/Baitap Reactjs/bai12/src/Components/ProductImages2.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProductImages2 extends Component{
+    render(){
+        return(
+            <div>
+                <h4>Image_2</h4>
+            </div>
+        )
+    };
+}
+
+export default ProductImages2;
---
+++ TuNK/Baitap Reactjs/bai12/src/Components/ProductImages3.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProductImages3 extends Component{
+    render(){
+        return(
+            <div>
+                <h4>Image_3</h4>
+            </div>
+        )
+    };
+}
+
+export default ProductImages3;
