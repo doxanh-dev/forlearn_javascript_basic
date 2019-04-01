8967071163c301ab24a83005131e5e412e3a3027
Nguyen Kim Tu <tunk@doxanh.edu.vn>
2019-04-02 00:49:00 +0700

    em co tạo hàm onchange khi nhập dữ liệu vào input thì giá trị đc set vào state, Tuy nhiên radio và checkbox em dùng formantic thì hàm onchange chưa get đc dữ liệu

--- TuNK/Baitap Reactjs/bt-project-web-1/src/containers/PostFormContainer.js
+++ TuNK/Baitap Reactjs/bt-project-web-1/src/containers/PostFormContainer.js
@@ -5,6 +5,7 @@ class PostFormContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            data: {},
             hobby: [
                 { value: "1", label: "Music" },
                 { value: "2", label: "Sport" },
@@ -13,7 +14,39 @@ class PostFormContainer extends Component {
             ]
         }
         this.returnHome = this.returnHome.bind(this);
+        this.onChangeText = this.onChangeText.bind(this);
     }
+
+    componentWillMount() {
+        this.setState({ data: this.props.data });
+    }
+
+    onChangeText(event) {
+        var newData = this.state.data;
+        var target = event.target;
+        var name = target.name;
+        newData[name] = target.value;
+        this.setState({
+            data: newData
+        });
+        console.log("data: " + JSON.stringify(this.state.data))
+    }
+
+    onChangeCheckbox(event, value) {
+        console.log("hello")
+        var index = this.state.data.hobby.indexOf(value);
+        const data = this.state.data;
+        if (index > -1) {
+            let newArr = data.hobby.filter(e => e !== value);
+            data[event.target.name] = newArr;
+        }
+        else {
+            let newArr = data.hobby.concat(value);
+            data[event.target.name] = newArr;
+        }
+        this.setState({ data: data });
+    }
+
     returnHome(event) {
         event.preventDefault();
         this.props.history.replace({
@@ -30,7 +63,13 @@ class PostFormContainer extends Component {
             return (
                 <div className="field" key={object.value}>
                     <div className="ui checkbox">
-                        <input type="checkbox" name="hobby" tabIndex="0" className="hidden" />
+                        <input type="checkbox"
+                            name="hobby"
+                            tabIndex="0"
+                            className="hidden"
+                            value={object.value}
+                            onChange={event => this.onChangeCheckbox(event, object.value)}
+                            checked={this.state.data.hobby.indexOf(object.value) > -1 ? true : false} />
                         <label>{object.label}</label>
                     </div>
                 </div>
@@ -48,21 +87,29 @@ class PostFormContainer extends Component {
                         <div className="two fields">
                             <div className="field">
                                 <label className="label-style">Fullname</label>
-                                <input type="text" name="fullname" placeholder="Fullname"></input>
+                                <input type="text"
+                                    name="fullname"
+                                    placeholder="Fullname"
+                                    value={this.state.data.fullname}
+                                    onChange={this.onChangeText} ></input>
                             </div>
                         </div>
                         <div className="two fields">
                             <div className="field">
                                 <label className="label-style">Address</label>
-                                <input type="text" name="address" placeholder="Address"></input>
+                                <input type="text"
+                                    name="address"
+                                    placeholder="Address"
+                                    value={this.state.data.address}
+                                    onChange={this.onChangeText} ></input>
                             </div>
                         </div>
                         <div className="three fields">
                             <div className="field">
                                 <label className="label-style">Class</label>
-                                <select name="class">
+                                <select name="class" onChange={this.onChangeText} value={this.state.data.class}>
                                     <option value="">---</option>
-                                    <option value="CNTT1">CNTT1</option>
+                                    <option value="CNTT1" >CNTT1</option>
                                     <option value="CNTT2">CNTT2</option>
                                     <option value="CNTT3">CNTT3</option>
                                     <option value="CNTT4">CNTT4</option>
@@ -75,13 +122,21 @@ class PostFormContainer extends Component {
                                 <div className="inline fields">
                                     <div className="field">
                                         <div className="ui radio checkbox ">
-                                            <input type="radio" name="gender" tabIndex="0" className="hidden" value="male" />
+                                            <input type="radio"
+                                                name="gender"
+                                                value="male"
+                                                onChange={this.onChangeText}
+                                                checked={this.state.data.gender === "male" ? true : false} />
                                             <label>Male</label>
                                         </div>
                                     </div>
                                     <div className="field">
                                         <div className="ui radio checkbox">
-                                            <input type="radio" name="gender" tabIndex="0" className="hidden" value="female" />
+                                            <input type="radio"
+                                                name="gender"
+                                                onChange={this.onChangeText}
+                                                value="female"
+                                                checked={this.state.data.gender === "female" ? true : false} />
                                             <label>Female</label>
                                         </div>
                                     </div>
@@ -96,8 +151,12 @@ class PostFormContainer extends Component {
                         </div>
                         <div className="field">
                             <label className="label-style">Notes</label>
-                            <textarea rows="5" name="note"></textarea>
+                            <textarea rows="5"
+                                name="note"
+                                value={this.state.data.note}
+                                onChange={this.onChangeText} ></textarea>
                         </div>
+                        <input type="hidden" name="id" value={this.state.data.id} ></input>
                         <div className="uk-text-left">
                             <button type="submit" className="ui telegram button">
                                 <i className="plus icon"></i>
@@ -117,7 +176,6 @@ class PostFormContainer extends Component {
 }
 
 const mapStateToProps = state => {
-    console.log("state: "+ JSON.stringify(state))
     return {
         data: Object.assign({}, state.data)
     }
